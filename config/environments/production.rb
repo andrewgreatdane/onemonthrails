8c99d525fe1a4bec3a083501cff5e0a65f8a896b
Andrew Mckee <andrewjmckee@gmail.com>
2016-05-08 13:43:20 -0400

    Implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for heroku
+  # note to set this to actual host
+  config.action_mailer.default_url_options = { host: 'https://omr-guide.herokuapp.com/'}
+
+
 end
